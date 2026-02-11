a99afee82bc3dc325678053237d6a807893ab34b
ttartungkam <91921552+ttartungkam@users.noreply.github.com>
2026-02-11 22:35:33 +0700

    add get_video_ids

--- video_stats.py
+++ video_stats.py
@@ -7,7 +7,9 @@ load_dotenv(dotenv_path="./.env")
 
 API_KEY = os.getenv("API_KEY")
 CHANNEL_HANDLE = os.getenv("CHANNEL_HANDLE")
+MAX_RESULTS = 50
 
+# Get the playlist ID for the channel's uploads
 def get_playlist_id():
     try:
 
@@ -29,5 +31,34 @@ def get_playlist_id():
     except requests.exceptions.RequestException as e:
         raise e
     
+def get_video_ids(playlistId):
+
+    base_url = f"https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={MAX_RESULTS}&playlistId={playlistId}&key={API_KEY}"
+    video_ids = []
+    pageToken = None
+
+    try:
+        while True:
+            paged_url = f"{base_url}&pageToken={pageToken}" if pageToken else base_url
+                
+            response = requests.get(paged_url)
+            response.raise_for_status()
+            data = response.json()
+
+            for item in data.get("items", []):
+                video_id = item["contentDetails"]["videoId"]
+                video_ids.append(video_id)  
+                
+            pageToken = data.get("nextPageToken")
+
+            if not pageToken:
+                break
+
+        return video_ids
+
+    except requests.exceptions.RequestException as e:
+        raise e
+    
 if __name__ == "__main__":
-    get_playlist_id()
+    playlistId = get_playlist_id()
+    get_video_ids(playlistId)
