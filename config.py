dc33a444068bb2e9974e64fefbfaab1e1818a2bf
ttartungkam <91921552+ttartungkam@users.noreply.github.com>
2026-02-12 15:41:24 +0700

    add config for call .env

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path="./.env")
+API_KEY = os.getenv("API_KEY")
+CHANNEL_HANDLE = os.getenv("CHANNEL_HANDLE")
+YT_URL = os.getenv("YT_URL")
+BATCH_SIZE = int(os.getenv("BATCH_SIZE", 50))
+MAX_RESULTS = int(os.getenv("MAX_RESULTS", 50))
