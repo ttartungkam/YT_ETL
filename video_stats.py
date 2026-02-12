346a02c50507127d7be67bf6a9a927aa42580e8d
ttartungkam <91921552+ttartungkam@users.noreply.github.com>
2026-02-12 13:32:33 +0700

    add batch_list & get_videos_details

--- video_stats.py
+++ video_stats.py
@@ -58,7 +58,53 @@ def get_video_ids(playlistId):
 
     except requests.exceptions.RequestException as e:
         raise e
+
+def batch_lists(input_list, batch_size):
+    for i in range(0, len(input_list), batch_size):
+        yield input_list[i:i + batch_size]
+
+def get_video_details(video_ids):
+    base_url = f"https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&key={API_KEY}"
+    video_details = []
+    pageToken = None
+
+    try:
+        while True:
+            for batch in batch_lists(video_ids, 50):
+                ids_string = ",".join(batch)
+                url = f"{base_url}&id={ids_string}"
+
+                response = requests.get(url)
+                response.raise_for_status()
+                data = response.json()
+
+                for item in data.get("items", []):
+                    video_info = {
+                        "videoId": item["id"],
+                        "title": item["snippet"]["title"],
+                        #"description": item["snippet"]["description"],
+                        "publishedAt": item["snippet"]["publishedAt"],
+                        "viewCount": item["statistics"].get("viewCount", 0),
+                        "likeCount": item["statistics"].get("likeCount", 0),
+                        "commentCount": item["statistics"].get("commentCount", 0)
+                    }
+                    video_details.append(video_info)
+                
+                pageToken = data.get("nextPageToken")
+
+                if not pageToken:
+                    break
+
+            return video_details
+
+    except requests.exceptions.RequestException as e:
+        raise e
+    
     
 if __name__ == "__main__":
     playlistId = get_playlist_id()
-    get_video_ids(playlistId)
+    video_ids = get_video_ids(playlistId)
+    video_details = get_video_details(video_ids)
+
+    print(json.dumps(video_details, indent=4))
+    print(f'Total videos fetched: {len(video_details)}')
